f83bcbfaccee8dca12ba45f343d87217ce80f53f
jlammott <jlammott@am.sony.com>
2019-08-09 17:06:01 -0700

    null attribute values in dictionaries

--- src/main/scala/org/apache/spark/datasources/dynamodb/writer/DynamodbDataWriter.scala
+++ src/main/scala/org/apache/spark/datasources/dynamodb/writer/DynamodbDataWriter.scala
@@ -8,8 +8,8 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import scala.collection.JavaConverters._
 import org.apache.spark.datasources.dynamodb.DynamoClient
 import org.apache.spark.sql.types.{DataType, DoubleType, FloatType, IntegerType, LongType, MapType, StringType, StructType, BooleanType, ArrayType}
-//import org.apache.spark.sql.types.MapType
 import org.apache.spark.unsafe.types.UTF8String
+import org.apache.spark.sql.catalyst.expressions.UnsafeMapData
 
 class DynamodbDataWriter(val table: String, val region: String, val schema: StructType) extends DataWriter[InternalRow] {
 
@@ -37,34 +37,48 @@ class DynamodbDataWriter(val table: String, val region: String, val schema: Stru
     
   }
 
-  def mapFieldType(item: Any, field: DataType): Option[AttributeValue.Builder] = {
+  def mapFieldType(item: AnyRef, field: DataType, nullable: Boolean = false): Option[AttributeValue.Builder] = {
     val builder = AttributeValue.builder()
     if (item==null)
-      return None
+      return if (nullable) Some(builder.nul(true)) else None
 
     field match {
       case BooleanType => Some(builder.bool(item.asInstanceOf[Boolean]))
       case ArrayType(innerType, _) => {
-        val items = item.asInstanceOf[Seq[_]].map(e => mapFieldType(e, innerType)).collect({
+
+        val items = item.asInstanceOf[Seq[AnyRef]].map(e => mapFieldType(e, innerType)).collect({
           case Some(x) => x.build()
         })
         Some(builder.l(items:_*))
       }
       case StringType => {
-        if (item.asInstanceOf[UTF8String].toString == "")
-          return None
+        if (item.asInstanceOf[UTF8String].toString.isEmpty)
+          return if (nullable) Some(builder.nul(true)) else None
         Some(builder.s(item.asInstanceOf[UTF8String].toString))
       }
       case FloatType | DoubleType | LongType | IntegerType => Some(builder.n(item.toString))
       case MapType(keyType, valueType, _) => {
 
+        println("help me out hereeee")
+        println(item.getClass.toString)
+        println(item.getClass.toString)
+        println(item.getClass.toString)
+        println(keyType.getClass.toString)
+        println(valueType.getClass.toString)
+
         if (keyType != StringType) throw new IllegalArgumentException(
           s"Invalid Map key type '${keyType.typeName}'. DynamoDB only supports String as Map key type.")
 
-        val map = item.asInstanceOf[Map[String, _]].mapValues(e => mapFieldType(e, valueType)).collect({
-          case (s, Some(v)) => (s, v.build())
+        val unsafeMap = item.asInstanceOf[UnsafeMapData]
+        val elements = (0 until unsafeMap.numElements()).map(i => {
+          //(i.toString, Some(builder.n(i.toString)))
+          val v = unsafeMap.valueArray().get(i, valueType)
+          (unsafeMap.keyArray().getUTF8String(i).toString, mapFieldType(v, valueType, true))
+        }).collect({
+          case (k, Some(v)) => (k, v.build())
         })
-        Some(builder.m(map.asJava))
+
+        Some(builder.m(elements.toMap.asJava))
       }
     }
   }
